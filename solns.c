9377e13d59003e4cca003deedc8c6705cd1c9710
govindr1310 <govindr1310@gmail.com>
2021-06-13 13:32:21 +0530

    initial commit

--- solns.c
+++ solns.c
@@ -1,2 +1,31 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include<cs50.h>
+/*max function*/
+int max(int a[],int n)
+{
+	int max=0;
+	for (int i=0;i<n;i++)
+	{
+		if (a[i]>max)
+			max=a[i];
+		else
+			continue;
+	}
+	return max;
+}
+/*min function*/
+int min(int a[],int n)
+{
+	int min=a[0];
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]<min)
+			min=a[i];
+		else
+			continue;
+	}
+	return min;
+}
+
+			
--- test_min.c
+++ test_min.c
@@ -12,3 +12,4 @@ int main(void) {
   assert (min(y, 1) == 5);
   printf("Min: passed\n");
   }
+
